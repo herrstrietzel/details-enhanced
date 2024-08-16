955be4b7f45283333e819c2e6e46c5ffe1a54ad1
herrstrietzel <36772754+herrstrietzel@users.noreply.github.com>
2024-08-16 02:22:34 +0200

    Update details-enhanced.js

--- v1/details-enhanced.js
+++ v1/details-enhanced.js
@@ -1,21 +1,37 @@
-
-/**
-* define custom SVG icons
-*/
-const summaryIcons = {
-    'arrow-right': `<svg viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14m-7-7 7 7-7 7"/></svg>`,
+if(typeof summaryIcons==='undefined'){
+    summaryIcons = {
+        'arrow-right': `<svg viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14m-7-7 7 7-7 7"/></svg>`,
+        'chevron': `<svg viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="m9 18 6-6-6-6"/></svg>`
+    }
 }
+
+// define default options
+if(typeof detailsDefaults==='undefined'){
+    detailsDefaults = {
+        target: 'body',
+        icon: '',
+        round: false,
+        right: false
+    }
+}
+
 
 /**
 * auto init by class name or data-attributes
 */
-let detailsToEnhance = document.querySelectorAll('.details-enhanced, [data-details]');
-if (detailsToEnhance.length) {
-    //console.log(detailsToEnhance);
-    enhanceDetails()
-}
+
+window.addEventListener('DOMContentLoaded', (e) => {
+
+    let detailsToEnhance = document.querySelectorAll('.details-enhanced, [data-details]');
+
+    if (detailsToEnhance.length) {
+        enhanceDetails(detailsDefaults)
+    }
+})
 
 function enhanceDetails(options = {}) {
+
+    console.log(summaryIcons);
     // default options
     options = {
         ...{
@@ -28,6 +44,7 @@ function enhanceDetails(options = {}) {
         ...options
     }
     let { target, icon, round, right } = options;
+
 
     // selector el
     let selection = document.querySelector(target);
@@ -134,6 +151,10 @@ function enhanceDetails(options = {}) {
             }
         }
 
+
+        //console.log(options);
+
+
         options = {
             ...options,
             ...summaryOptions,
@@ -142,6 +163,8 @@ function enhanceDetails(options = {}) {
 
         //extract final options
         let { target, icon, round, right, plus } = options;
+
+
 
         /** 
          * add toggle icon
